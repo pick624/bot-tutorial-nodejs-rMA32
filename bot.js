e234dfe4d22200852e958abc9ecbd6d06322fc0d
pick624 <imupfor6@gmail.com>
2016-04-25 13:23:11 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy/;  botRegexDL = /^\/ddl/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/;
       botRegexAd=/^\/advance/;botRegexMI = /^\/Mike/; botRegexSC = /^\/SZN/i; botDEZ = /^\/Dez/; botDuck = /^\/duck/;
-      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSB = /^\/schedules/; botRegexSh = /^\/shrug/; botRegexWk = /^\/teams/; botRegexCC = /^\/cc/;
+      botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSB = /^\/full rules/; botRegexSh = /^\/shrug/; botRegexWk = /^\/teams/; botRegexCC = /^\/cc/;
       botRegexCryNeema=/^\/cry neema/; botRegexCheese = /^\/cheese/
       cheese1 = 'https://pbs.twimg.com/profile_images/494330891/cheese_oh_cheese_400x400.jpg'; cheese2 = 'https://i.ytimg.com/vi/efLRmlILC2I/maxresdefault.jpg'
       cheese3 = 'http://www.wallydfantasyfootball.com/things/cheddar-cheese-cheesehead-fantasy-football-logo-helmet.jpg';
@@ -36,52 +36,7 @@ function respond() {
   }
   else if(request.text && botRegexRules.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("RULES & GUIDELINES
-
-•    QUITTING:   No Quitting! If you quit it is an Automatic removal from the league.
-•    SIM STYLE: This is a SIM LEAGUE! Not a league made to search the internet looking for exploits and "money" plays. Mix up your calls and play realistic!
-•    DISCONNECTION POLICY:   If a game gets disconnected, restart the game. No excuses! You may take the win only if:
-•    1st half: person whose connection drops was losing by 21 or more points
-•    3rd Q: person whose connection drops is losing by 17 or more points
-•    4th Q: person whose connection drops is losing by 10 or more points
-•    NO TANKING:   Members are not allowed to throw a season or game to have a higher draft pick.
-•    SPORTSMANSHIP:   If you are winning big in the 4th: Don’t try and embarrass your opponent by calling timeouts and trying to run up the score If your opponent is out of time outs and game is under 2 minutes, kneel the ball. (This can also be called stats padding and will result in player suspension)
-•    if you're up 28+ you must chew clock no matter what quarter it is. You can stop chew clock if the score gets closer. (Your game score should NOT be 65+ points)
-•    CHEW CLOCK: You can start chewing clock IF: 1) it's 4th quarter and 2 mins left. 2) If you are winning by a lot (28+ points) 3) If your opponent gives up and allows you to chew clock 
- GAME PLAY OFFENSE
-
-4th DOWN RULE:   These are the only situations in a game when you can go for it on 4th down: (No more than 1 per drive unless you guy losing more than 14)
-•    FIRST HALF:
-4th down in your own territory (0-50 yard line) Punt the ball.
-4th and 1 or less in your opponent’s territory (49-0) you are allowed to go for it.
-4th and goal inside the 5 yard line you are allowed to go for it
-•    3rd Quarter
-Same rules apply as first half unless you are losing by 14 points or more then you are allowed to go for it anytime and anywhere. (At own risk)
-•    4th Quarter
-If you are losing then you are allowed to go for it anytime and anywhere. If the game is tied then you can go for it at own risk. Also if you are winning by 8 or less and its 4th and 1 or inches then you are allowed to go for it only on opponent’s side of the field (49-0)
-•    OT: You are allowed to go for it anytime.
-•    Hurry Up Rule - You are allowed to run no huddle for 2 consecutive plays. If you are losing by 14 or more anytime in the game then you can hurry up all you want. Also under 2 minutes in half or 4th quarter hurry up all you want.
-•    2PT CONVERSION: You can go for 2pts only when you need them in a real situation. You can also go for it to win a game at the end
-•    QB DROP:   You cannot purposely drop back with your QB more than 10 yards after the snap. Also no running straight to sideline when you hike the ball to buy more time. That is not SIM play. You can rollout with QB and run or throw immediately. If you are caught time after time doing this you will be removed.
-•    SCREENS:   Maximum of 6 screens per game
-•    PUNTS:   You cannot drop a player back into punt coverage before the snap
-•    FAKES: 1 FAKE on special teams is allowed per game (Includes onside unless losing in the 4th quarter)
-•    QB ROLL OUTS: Do not abuse the QB roll out to lead the WR. Example: you roll out right away after snapping the ball (unless it’s a design play) to lead the WR. If you roll out pass the numbers, you HAVE to run the ball. (Unless it’s a design play/or you pressure in the box)
-•    QB SCRAMBLE: No excessive QB scrambling.  Example: 4 WR Verticals play, after hiking you scramble right away multiple times per drive.
-•    PLAY MAKER IS BANNED Due to people cheese while rolling out, play maker is banned from this league, if caught, you will be suspended!
-
-
-GAME PLAY DEFENSE
-
-•    No manually moving a bunch of players all over the field. You are allowed to move a player in COVERAGE in a realistic manner to line up over a receiver; this means a couple of feet here and there.
-•    If you use a D-lineman and want to go out in a zone or cover, you must stay on the line until the ball is hiked and then you may drop back.
-•    BLITZING:  Nano blitzes are NOT allowed
-•    SHIFTS:  :  Excessive shifts (back & forth) are not allowed (shake blitz)
-•    COVERAGE:  :  When USER controlling a defender in pass coverage, you cannot purposely bump receivers down field after 5 yards. If you do it by mistake, back off and do not continue to bump the offensive player. This is still a major flaw in the game and it is a major disadvantage to the offense.
-•       SUPER ZONE RULE:  :  You must rush 3 or more players on defense at all times! It does NOT have to be 3 D-Lineman. It can be any combination of players on defense. 
-Please use the 80/20 rule, either 80% passes and 20% run or 80% run and 20% pass.  This is the same for DEFENSE too (80% zone, 20% man, etc.)  We don’t care, as long as you are not abusing the same plays. Mix it up! Just don’t be calling the same run play or same pass play multiple times per drive. There are tons of different run plays and pass plays. This should not be an issue. 
-
-");
+    postMessage("https://i.groupme.com/1992x2629.jpeg.dd767f0d9f854ea9a872c2113a988d2a");
     this.res.end();
   } 
   else if(request.text && botRegexMI.test(request.text)) {
@@ -109,7 +64,7 @@ Please use the 80/20 rule, either 80% passes and 20% run or 80% run and 20% pass
   } 
   else if(request.text && botRegexSB.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/maddenszn/schedule/s");
+    postMessage("http://daddyleagues.com/maddenszn/rules");
     this.res.end();
   } 
   else if(request.text && botRegexSh.test(request.text)) {
