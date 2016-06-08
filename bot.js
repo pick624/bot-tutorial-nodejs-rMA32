7f27195e02321dbce573cee38812278b3e3fca1e
pick624 <imupfor6@gmail.com>
2016-06-07 20:58:06 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ function respond() {
   } 
   else if(request.text && botRegexAd.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.hackcollege.com/wp-content/uploads/2013/02/kno_advance.jpg");
+    postMessage("https://i.imgflip.com/xgtsl.jpg");
     this.res.end();
   }
   else if(request.text && botRegexRules.test(request.text)) {
